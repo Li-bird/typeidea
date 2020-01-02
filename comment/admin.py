6af141ce0d23d453e895df5ecd4f1f92be781ee0
LXX <2324637471@qq.com>
2020-01-02 11:59:58 +0800

    blog

--- comment/admin.py
+++ comment/admin.py
@@ -1,3 +1,30 @@
 from django.contrib import admin
 
 # Register your models here.
+from comment.models import Comment
+from config.models import Link, SideBar
+
+
+@admin.register(Comment)
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ('target', 'nickname', 'content', 'website', 'created_time')
+
+
+@admin.register(Link)
+class LinkAdmin(admin.ModelAdmin):
+    list_display = ('title', 'href', 'status', 'weight', 'created_time')
+    fields = ('title', 'href', 'status', 'weight')
+
+    def save_model(self, request, obj, form, change):
+        obj .owner = request.user
+        return  super(LinkAdmin, self).save_model(request, obj, form, change)
+
+
+@admin.register(SideBar)
+class SideBarAdmin(admin.ModelAdmin):
+    list_display = ('title', 'display_type', 'content', 'created_time')
+    fields = ('title', 'display_type', 'content')
+
+    def save_model(self, request, obj, form, change):
+        obj.owner = request.user
+        return super(SideBarAdmin, self).save_model(request, obj, form, change)
