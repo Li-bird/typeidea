c44ee6115624b240ba34505147953ea38a281758
LXX <2324637471@qq.com>
2020-01-04 12:00:48 +0800

    SideBar

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
-from blog.models import Tag, Post
+from blog.models import Tag, Post, Category
+from config.models import SideBar
 
 '''
 def post_list(request, category_id=None, tag_id=None):
@@ -15,8 +16,12 @@ def post_list(request, category_id=None, tag_id=None):
     
 def post_detail(request, post_id):
     return HttpResponse('detail')
-    '''
+'''
+
 def post_list(request, category_id=None, tag_id=None):
+    tag = None
+    category = None
+    '''
     if tag_id:
         try:
             tag = Tag.objects.get(id=tag_id)
@@ -27,13 +32,38 @@ def post_list(request, category_id=None, tag_id=None):
     else:
         post_list = Post.objects.filter(status=Post.STATUS_NORMAL)
         if category_id:
-            post_list = post_list.filter(category_id=category_id)
+            try:
+                category = Category.objects.get(id=category_id)
+            except Category.DoesNotExist:
+                category = None
+            else:
+                post_list = post_list.filter(category_id=category_id)
+    '''
+    if tag_id:
+        post_list, tag = Post.get_by_tag(tag_id)
+    elif category_id:
+        post_list, category = Post.get_by_category(category_id)
+    else:
+        post_list = Post.latest_posts()
 
-    return render(request, 'blog/list.html', context={'post_list': 'post_list'})
+    context ={
+        'category': category,
+        'tag': tag,
+        'post_list': post_list,
+        'sidebars': SideBar.get_all()
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/list.html', context=context)
 
 def post_detail(request, post_id=None):
     try:
         post = Post.objects.get(id=post_id)
     except Post.DoesNotExist:
         post = None
-    return render(request, 'blog/detail.html', context={'post_detail': 'post_detail'})
+
+    context ={
+        'post': post,
+        'sidebars': SideBar.get_all()
+    }
+    context.update(Category.get_navs())
+    return render(request, 'blog/detail.html', context=context)
